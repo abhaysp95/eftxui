0ae78ea0d0fa90a9c661ab584de3a75131cceef7
abhay <abhaysp95@protonmail.com>
2023-02-21 22:40:57 +0530

    feat: flexbox basic from ftxui

---
+++ src/layout_flexbox_basic.cpp
@@ -0,0 +1,48 @@
+#include <cstdlib>
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/dom/node.hpp>
+#include <ftxui/screen/screen.hpp>
+#include <iterator>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+  using namespace ftxui;
+
+  auto make_box = [](int dimx, int dimy) {
+    std::string title = std::to_string(dimx) + "x" + std::to_string(dimy);
+    return window(text(title) | hcenter | bold,
+                  text("content") | hcenter | dim) |
+           size(WIDTH, EQUAL, dimx) | size(HEIGHT, EQUAL, dimy);
+  };
+
+  auto style = size(WIDTH, GREATER_THAN, 20) | border | size(HEIGHT, GREATER_THAN, 20) | size(WIDTH, LESS_THAN, 50);
+
+  auto document = hflow({
+                      make_box(7, 7),
+                      make_box(7, 5),
+                      make_box(5, 7),
+                      make_box(10, 4),
+                      make_box(10, 4),
+                      make_box(10, 4),
+                      make_box(10, 4),
+                      make_box(11, 4),
+                      make_box(11, 4),
+                      make_box(11, 4),
+                      make_box(11, 4),
+                      make_box(12, 4),
+                      make_box(12, 5),
+                      make_box(12, 4),
+                      make_box(13, 4),
+                      make_box(13, 3),
+                      make_box(13, 3),
+                      make_box(10, 3),
+		  }) | style;
+
+  auto screen = Screen::Create(Dimension::Full(), Dimension::Fit(document));
+  Render(screen, document);
+  screen.Print();
+
+  return EXIT_SUCCESS;
+}
